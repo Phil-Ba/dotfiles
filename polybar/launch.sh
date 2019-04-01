ffaa5070b0d0e7b374a9dd9b545da1875a5aaf51
philBa <p.bayer@freemail.hu>
2019-04-01 09:58:28 +0200

    Added support for multiple monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -c ~/.config/polybar/config main &
+#polybar -c ~/.config/polybar/config main &
+# Solution from https://github.com/jaagr/polybar/issues/763 to polybar in
+# multiple monitors
+for monitor in $(polybar --list-monitors | cut -d ":" -f 1); do
+  # Launch bar1 and bar2
+  MONITOR=$monitor polybar --reload --config=$HOME/.config/polybar/config main &
+#  MONITOR=$monitor polybar --reload --config=$HOME/.config/polybar/config bottom &
+
+done
+
 
 echo "Polybar launched..."
