8dcea26ef4790da7cc53423ca79c85ad052af497
philBa <p.bayer@freemail.hu>
2019-04-02 10:43:35 +0200

    Added screenlayouts

---
+++ .screenlayout/work-3-monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 3840x120 --rotate normal --output DP1 --off --output DVI-I-3-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DVI-I-2-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
